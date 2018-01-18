77f82de0d676817b47028cda61ef8d0171be5b86
maomao <you@example.com>
2018-01-18 15:22:58 +0800

    给supervisor一个名称

--- demo_sup/lib/demo_sup/my_supervisor.ex
+++ demo_sup/lib/demo_sup/my_supervisor.ex
@@ -1,17 +1,22 @@
 defmodule DemoSup.MySupervisor do
-  use Supervisor
+	use Supervisor
 
-  def start_link() do
-    	Supervisor.start_link(__MODULE__, [])  # 会调用 init 回调
-  end
+	def start_link() do
+		Supervisor.start_link(__MODULE__, [], name: __MODULE__)  # 会调用 init 回调
+	end
 
-  def init(_arg) do
-  	import Supervisor.Spec
 
-	children = [
-	 	worker(DemoSup.UnnameServer, [], restart: :temporary)
-	]
+	def start_child() do 
+		Supervisor.start_child(__MODULE__, [])
+	end
 
-    	supervise(children, strategy: :simple_one_for_one)
-  end
+	def init(_arg) do
+		import Supervisor.Spec
+
+		children = [
+		 	worker(DemoSup.UnnameServer, [], restart: :temporary)
+		]
+
+		supervise(children, strategy: :simple_one_for_one)
+	end
 end
