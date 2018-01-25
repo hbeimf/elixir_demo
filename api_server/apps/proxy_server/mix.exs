cc47abe01c6412a6897612c73cfc5c257165a368
maomao <you@example.com>
2018-01-25 11:30:25 +0800

    deps

--- api_server/apps/proxy_server/mix.exs
+++ api_server/apps/proxy_server/mix.exs
@@ -32,10 +32,9 @@ defmodule ProxyServer.Mixfile do
       {:ranch, "~> 1.3.2"},
       # {:mysqlc, in_umbrella: true},
       # {:redisc, in_umbrella: true},
+      {:sys_config, path: "../../erlang_apps/sys_config"},
       {:mysqlc, path: "../../erlang_apps/mysqlc"},
       {:redisc, path: "../../erlang_apps/redisc"},
-      # {:rconf, path: "../../erlang_apps/rconf"},
-      {:sys_config, path: "../../erlang_apps/sys_config"},
       {:table, path: "../../erlang_apps/table"}
       # {:sync, "~> 0.1.3"}
     ]
