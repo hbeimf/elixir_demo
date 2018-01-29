9928b474c53a0610421a7d1ca653707654148365
maomao <you@example.com>
2018-01-29 18:21:51 +0800

    c

--- proxy/apps/tcpc/src/tcpc_sup.erl
+++ proxy/apps/tcpc/src/tcpc_sup.erl
@@ -13,6 +13,7 @@
 %% Supervisor callbacks
 -export([init/1]).
 
+
 -define(SERVER, ?MODULE).
 
 %%====================================================================
@@ -27,8 +28,31 @@ start_link() ->
 %%====================================================================
 
 %% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+% init([]) ->
+%     {ok, { {one_for_all, 0, 1}, []} }.
+% https://github.com/devinus/poolboy
 init([]) ->
-    {ok, { {one_for_all, 0, 1}, []} }.
+    % {ok, Pools} = application:get_env(example, pools),
+    Pools = [
+    	{pool1, [{size, 10},{max_overflow, 20}], []}
+    	% ,
+            % {pool2, [{size, 5}, {max_overflow, 10}], 
+            % [
+            %     {hostname, "127.0.0.1"},
+            %     {database, "db2"},
+            %     {username, "db2"},
+            %     {password, "abc123"}
+            % ]}
+    ],
+
+
+    PoolSpecs = lists:map(fun({Name, SizeArgs, WorkerArgs}) ->
+        PoolArgs = [{name, {local, Name}},
+            		{worker_module, tcpc_work}] ++ SizeArgs,
+        poolboy:child_spec(Name, PoolArgs, WorkerArgs)
+    end, Pools),
+    {ok, {{one_for_one, 10, 10}, PoolSpecs}}.
+
 
 %%====================================================================
 %% Internal functions
