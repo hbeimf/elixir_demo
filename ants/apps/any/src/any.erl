20a3193183b4982db7d5781af6d12a8dfcb01284
maomao <you@example.com>
2018-04-12 11:50:25 +0800

    any

--- ants/apps/any/src/any.erl
+++ ants/apps/any/src/any.erl
@@ -2,12 +2,10 @@
 -compile(export_all).
 
 % http://blog.sina.com.cn/s/blog_510844b70102wrvf.html
-
 % http://hq.sinajs.cn/list=sh601006
 
 test() -> 
 	get_data(<<"sh601006">>).
-
 
 get_data(Code) -> 
 	Url = lists:concat(["http://hq.sinajs.cn/list=", glib:to_str(Code)]),
@@ -15,10 +13,13 @@ get_data(Code) ->
 		<<"">> ->
 			ok;
 		Body ->
-			body(Body)
+			body(Code, Body)
 	end.
 
+body(_Code, Body) -> 
+	Str = glib:trim(glib:to_str(Body)),
+	[_, List|_] = glib:explode(Str, "="),
+	[_|Fileds] = glib:explode(List, ","),
+	Fileds.
 
-body(Body) -> 
 
-	Body.
