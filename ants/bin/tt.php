5ced46200a31725c331c2360120756c09bd83eb2
maomao <you@example.com>
2018-04-19 14:47:31 +0800

    x

--- ants/bin/tt.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// $url = "http://quotes.money.163.com/service/chddata.html?code=1002566&start=20150104&end=20160108";
-$url = "http://quotes.money.163.com/service/chddata.html?code=1002566&start=20000104&end=20160108";
-$con = file_get_contents($url);
-$con = iconv("GBK", "UTF-8", $con);
-echo file_put_contents("./data.cvs", $con);
-
-?>
