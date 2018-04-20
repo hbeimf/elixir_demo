c2b86e8aeff9f36612c3a4e2dfc055462fa6dc32
maomao <you@example.com>
2018-04-20 14:47:35 +0800

    x

--- ants/bin/phper/run.php
+++ ants/bin/phper/run.php
@@ -1,14 +1,58 @@
 <?php
 $loader = require './vendor/autoload.php';
 
+// http://quotes.money.163.com/0600000.html#9b01
 // use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Database\Capsule\Manager as DB;
 
 class get_data {
 	public function run() {
-		$data = DB::table('m_gp_list')->first();
-		print_r($data);
+		$data = DB::table('m_gp_list')->get();
+		$i = 0;
+		foreach ($data as $v) {
+			// echo $v['code'] . "\n";
+			// echo substr($v['code'], 2, 6) . "\n";
+			$code = '0' . substr($v['code'], 2, 6);
+			$code1 = '1' . substr($v['code'], 2, 6);
+
+			$this->download($code);
+			$this->download($code1);
+
+			// $code = '1002566';
+			// $code = '0600614';
+			// print_r($v);
+
+			// $today = date("Ymd", time());
+			// $link = $this->_url . "?code={$code}&start=20150101&end={$today}";
+			// // echo $link . "\n";
+			// $data = file_get_contents($link);
+			// echo iconv('GBK', 'utf-8', $data); //exit;
+			// file_put_contents("./csv/{$code}.csv", $data);
+
+			// if ($i >= 2) {
+			// 	break;
+			// }
+			// $i++;
+		}
+
 	}
+
+	public function download($code) {
+		$today = date("Ymd", time());
+		$link = $this->_url . "?code={$code}&start=20000101&end={$today}";
+		// echo $link . "\n";
+		$data = file_get_contents($link);
+		$data = iconv('GBK', 'utf-8', $data); //exit;
+
+		$dst = "./csv/{$code}.csv";
+		$size = file_put_contents($dst, $data);
+		echo $size;
+		if ($size < 200) {
+			unlink($dst);
+		}
+		echo "\n";
+	}
+
 	public function __construct() {
 		$this->_initDatabaseEloquent();
 	}
@@ -32,6 +76,8 @@ class get_data {
 		'collation' => 'utf8_unicode_ci',
 		'prefix' => "",
 	];
+
+	private $_url = "http://quotes.money.163.com/service/chddata.html";
 }
 
 $obj = new get_data();
