019102bdb2083af12a9e056bfecf2a67f903b370
maomao <you@example.com>
2018-05-24 17:44:53 +0800

    i

--- manager/application/library/Table/Logic/Code.php
+++ manager/application/library/Table/Logic/Code.php
@@ -62,16 +62,22 @@ class Table_Logic_Code extends EloquentModel {
 					$today_top_price = is_numeric($list[4]) ? $list[4] : 0;
 					$today_bottom_price = is_numeric($list[5]) ? $list[5] : 0;
 
+					$rise_and_fall_num = is_numeric($list[8]) ? $list[8] : 0;
+					$rise_and_fall_percent = is_numeric($list[9]) ? $list[9] : 0;
+					$turnover_rate = is_numeric($list[10]) ? $list[10] : 0;
+					$volume = is_numeric($list[11]) ? $list[11] : 0;
+					$transaction_amount = is_numeric($list[12]) ? $list[12] : 0;
+
 					if ($close_price == 0) {
 
 					} else {
-						$vals[] = "('{$from_code}', '{$code}', '{$name}', '{$timer}', {$timer_int}, {$open_price}, {$yesterday_close_price}, {$close_price}, {$today_top_price}, {$today_bottom_price})";
+						$vals[] = "('{$from_code}', '{$code}', '{$name}', '{$timer}', {$timer_int}, {$open_price}, {$yesterday_close_price}, {$close_price}, {$today_top_price}, {$today_bottom_price}, {$rise_and_fall_num}, {$rise_and_fall_percent}, {$turnover_rate}, {$volume}, {$transaction_amount})";
 					}
 				}
 			}
 
 			try {
-				$sql = "INSERT IGNORE INTO m_all (from_code, code, name, timer, timer_int, open_price, yesterday_close_price, close_price, today_top_price, today_bottom_price) values " . implode(', ', $vals);
+				$sql = "INSERT IGNORE INTO m_all (from_code, code, name, timer, timer_int, open_price, yesterday_close_price, close_price, today_top_price, today_bottom_price, rise_and_fall_num, rise_and_fall_percent, turnover_rate, volume, transaction_amount) values " . implode(', ', $vals);
 
 				// echo $sql;exit;
 				$r = DB::insert($sql);
